086501262580d6a7876238b36ed3b46dfce38dd7
ouzture <ouzture@gmail.com>
2020-03-27 16:07:46 +0300

    project init

---
+++ src/main/java/ouzture/springframework/recipator/RecipatorApplication.java
@@ -0,0 +1,13 @@
+package ouzture.springframework.recipator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ouzture/springframework/recipator/RecipatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ouzture.springframework.recipator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
