7eb8b21889e3010f42deb07fc1305d9b348330a8
ouzture <ouzture@gmail.com>
2020-04-11 09:50:45 +0300

    Added UnitOfMeasure.java

--- src/main/java/ouzture/springframework/recipator/domain/Ingredient.java
+++ src/main/java/ouzture/springframework/recipator/domain/Ingredient.java
@@ -13,7 +13,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    // private UnitOfMeasure uom;
+    @OneToOne
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -47,5 +49,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
