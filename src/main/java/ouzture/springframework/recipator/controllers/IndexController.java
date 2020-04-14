14624e13873ec76991fc37091718ea297d5561df
ouzture <ouzture@gmail.com>
2020-04-14 11:13:12 +0300

    Added data.sql for startup data

--- src/main/java/ouzture/springframework/recipator/controllers/IndexController.java
+++ src/main/java/ouzture/springframework/recipator/controllers/IndexController.java
@@ -2,12 +2,33 @@ package ouzture.springframework.recipator.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ouzture.springframework.recipator.domain.Category;
+import ouzture.springframework.recipator.domain.UnitOfMeasure;
+import ouzture.springframework.recipator.domain.repositories.CategoryRepository;
+import ouzture.springframework.recipator.domain.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category id : " + categoryOptional.get().getId());
+        System.out.println("Uom id: " + unitOfMeasure.get().getId());
+
 
         return "index";
     }
--- src/main/java/ouzture/springframework/recipator/domain/repositories/CategoryRepository.java
+++ src/main/java/ouzture/springframework/recipator/domain/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package ouzture.springframework.recipator.domain.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ouzture.springframework.recipator.domain.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
