870c6782b5391c1c1792ba17d65b9e762f3014a9
ouzture <ouzture@gmail.com>
2020-05-11 19:07:48 +0300

    added recipe save

--- src/main/java/ouzture/springframework/recipator/controllers/RecipeController.java
+++ src/main/java/ouzture/springframework/recipator/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package ouzture.springframework.recipator.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ouzture.springframework.recipator.commands.RecipeCommand;
 import ouzture.springframework.recipator.services.RecipeService;
 
 @Controller
@@ -19,5 +22,19 @@ public class RecipeController {
     public String showById(@PathVariable Long id, Model model){
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
