04f89611003462312028b6c496feb44dc16f52d4
ouzture <ouzture@gmail.com>
2020-05-14 17:00:26 +0300

    added view ingredient

--- src/main/java/ouzture/springframework/recipator/controllers/IngredientController.java
+++ src/main/java/ouzture/springframework/recipator/controllers/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ouzture.springframework.recipator.services.IngredientService;
 import ouzture.springframework.recipator.services.RecipeService;
 
 @Slf4j
@@ -13,19 +14,29 @@ import ouzture.springframework.recipator.services.RecipeService;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping
     @RequestMapping("/recipe/{recipeId}/ingredients")
-    public String listIngredients(@PathVariable Long recipeId, Model model){
+    public String listIngredients(@PathVariable String recipeId, Model model){
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id, Model model){
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+        return "recipe/ingredient/show";
     }
 }
---
+++ src/main/java/ouzture/springframework/recipator/services/IngredientService.java
@@ -0,0 +1,8 @@
+package ouzture.springframework.recipator.services;
+
+import ouzture.springframework.recipator.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
