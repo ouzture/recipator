9458d948b108bcac9b4bf1b8d33ea214fb750059
ouzture <ouzture@gmail.com>
2020-05-27 14:34:03 +0300

    Added not found page

--- src/main/java/ouzture/springframework/recipator/controllers/RecipeController.java
+++ src/main/java/ouzture/springframework/recipator/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package ouzture.springframework.recipator.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import ouzture.springframework.recipator.commands.RecipeCommand;
+import ouzture.springframework.recipator.exceptions.NotFoundException;
 import ouzture.springframework.recipator.services.RecipeService;
 
 @Slf4j
@@ -51,5 +54,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
